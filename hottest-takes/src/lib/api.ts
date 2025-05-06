019305101adb376f1a5f4bf9d1cb1e2c394511fb
Eduardo <eduardo.benigno15@gmail.com>
2025-05-06 11:23:09 -0300

    feat: preparing class 0201

--- hottest-takes/src/lib/api.ts
+++
@@ -1,9 +0,0 @@
-export const fetchUsers = async (): Promise<User[]> => {
-  const res = await fetch('/api/users');
-  return await res.json();
-};
-
-export const fetchPosts = async (): Promise<User[]> => {
-  const res = await fetch('/api/posts');
-  return await res.json();
-};
